7a17c31165735a647dbefdcae83fe375ad619907
Matthew Caron <matt@mattcaron.net>
2025-05-22 14:26:15 -0400

    Add missing documentation.

--- src/delme.rs
+++ src/delme.rs
@@ -1,9 +1,31 @@
-// This is just a POC. It should be deleted after real components with their respective test suites are added to the project.
+// Copyright (c) 2021-2025 José Manuel Barroso Galindo <theypsilon@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// You can download the latest version of this tool from:
+// https://github.com/theypsilon/downloader-rs
+
+//! The delme module. It should be deleted.
+//!
+//! This is just a POC. It should be deleted after real components with their
+//! respective test suites are added to the project.
 
 use std::rc::Rc;
 
 use crate::filesystem::*;
 
+/// A test service
 #[derive(Clone)]
 pub struct ServiceA {
     fs: Rc<dyn FileSystem>,
@@ -11,30 +33,44 @@ pub struct ServiceA {
 }
 
 impl ServiceA {
+    /// Create a new ServiceA with a given filesystem path and value
     pub fn new(fs: Rc<dyn FileSystem>, a: i32) -> Self {
         Self { fs, a }
     }
+
+    /// A test function
+    ///
+    /// Touches the file "a" in the stored filesystem path and returns the
+    /// stored value.
     pub fn a(&self) -> i32 {
         self.fs.touch("a");
         self.a
     }
 }
 
+/// Another test service
 pub struct ServiceB {
     fs: Rc<dyn FileSystem>,
     b: i32,
 }
 
 impl ServiceB {
+    /// Create a new ServiceB with a given filesystem path and value
     pub fn new(fs: Rc<dyn FileSystem>, b: i32) -> Self {
         Self { fs, b }
     }
+
+    /// Another test function
+    ///
+    /// Touches the file "b" in the stored filesystem path and returns the
+    /// stored value.
     pub fn b(&self) -> i32 {
         self.fs.touch("b");
         self.b
     }
 }
 
+/// A third test service
 pub struct ServiceC {
     fs: Rc<dyn FileSystem>,
     service_a: Rc<ServiceA>,
@@ -42,6 +78,8 @@ pub struct ServiceC {
 }
 
 impl ServiceC {
+    /// Create a new ServiceC with a given filesystem path, ServiceA and
+    /// ServiceB structs.
     pub fn new(fs: Rc<dyn FileSystem>, service_a: Rc<ServiceA>, service_b: Rc<ServiceB>) -> Self {
         Self {
             fs,
@@ -50,6 +88,10 @@ impl ServiceC {
         }
     }
 
+    /// Calculate the result
+    ///
+    /// Touches the file calc" in the stored filesystem path and returns the
+    /// result of ServiceA + ServiceB.
     pub fn calc(&self) -> i32 {
         self.fs.touch("calc");
         self.service_a.a() + self.service_b.b()
--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,10 +1,41 @@
+// Copyright (c) 2021-2025 José Manuel Barroso Galindo <theypsilon@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// You can download the latest version of this tool from:
+// https://github.com/theypsilon/downloader-rs
+
+//! Filesystem glue logic crate.
+
+/// Filesystem trait
+///
+/// Contains things filesystems should do.
 pub trait FileSystem {
+    /// Touch a file at a path given by `path`
+    ///
+    /// # Arguments
+    ///
+    /// * `touch` - file to touch
+    ///
     fn touch(&self, path: &str);
 }
 
 #[derive(Clone)]
+/// A real filesystem implementation
 pub struct RealFileSystem {}
 
+/// Default constructor
 impl Default for RealFileSystem {
     fn default() -> Self {
         Self::new()
@@ -12,6 +43,7 @@ impl Default for RealFileSystem {
 }
 
 impl RealFileSystem {
+    /// Create a new filesystem struct.
     pub fn new() -> Self {
         Self {}
     }
